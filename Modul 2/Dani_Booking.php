8cadda849586518481a4ccf68bc081642c934506
alfianmf <alfianm999@gmail.com>
2022-11-04 07:23:32 +0700

    debug

--- Modul 2/Dani_Booking.php
+++ Modul 2/Dani_Booking.php
@@ -1,141 +1,141 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-        <title>Dani Modul 2</title>
-    </head>
-    <style>
-        .nav-link {
-            color: white;
-        }
-        .nav-link:hover{
-            color: cyan;
-        }
-        .plisKeTengah{
-            margin-top: 25%;
-        }
-    </style>
-<body>
-<!-- Navbar -->
-<nav class="navbar navbar-expand-lg bg-dark" >
-    <div class="container-fluid">
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="nav"  style="margin-left: auto; margin-right: auto;">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="Dani_Home.php">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="Dani_Booking.php">Booking</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
-
-<!-- Judul -->
-<section>
-    <div class="container" style = "text-align : center;">
-    <br/>
-        <h4>Rent your car now!</h4>
-    </div>
-    <br/>
-    <br/>
-</section>
-
-<!-- Form dan Gambar -->
-<section>
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <div class="plisKeTengah">
-                    <img src="https://daihatsu-cilegon.com/wp-content/uploads/2019/03/Daihatsu-allnewterios.png" class="img" alt="Terios">
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+                <title>Dani Modul 2</title>
+            </head>
+            <style>
+                .nav-link {
+                    color: white;
+                }
+                .nav-link:hover{
+                    color: cyan;
+                }
+                .plisKeTengah{
+                    margin-top: 25%;
+                }
+            </style>
+        <body>
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg bg-dark" >
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="nav"  style="margin-left: auto; margin-right: auto;">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="Dani_Home.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="Dani_Booking.php">Booking</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
-            
-            <div class="col">
-                <form action="Dani_MyBooking.php" method="POST">
-                    <!-- Nama -->
-                    <div class="mb-3">
-                        <label for="nama" class="form-label">Name</label>
-                        <input type="text" class="form-control" id="nama" name="nama" value="DANI_1202202071" readonly enabled>
-                    </div>
+        </nav>
 
-                    <!-- Tanggal -->
-                    <div class="mb-3">
-                        <label for="bookDate" class="form-label">Book Date</label>
-                        <input type="date" class="form-control" id="bookDate" name="bookDate">
-                    </div>
-
-                    <!-- Waktu -->
-                    <div class="mb-3">
-                        <label for="startTime" class="form-label">Start Time</label>
-                        <input type="time" class="form-control" id="startTime" name="startTime">
-                    </div>
-
-                    <!-- Durasi -->
-                    <div class="mb-3">
-                        <label for="dur" class="form-label">Duration (Days)</label>
-                        <input type="number" class="form-control" id="dur" name="dur">
-                    </div>
-
-                    <!-- Pilih mobil -->
-                    <div class="mb-3">
-                        <label for="car" class="form-label">Car Type</label>
-                        <select class="form-select" aria-label="Default select example" name="car">
-                            <option selected>Toyota Avanza</option>
-                            <option value="Daihatsu Terios">Daihatsu Terios</option>
-                            <option value="Honda Brio">Honda Brio</option>
-                        </select>
-                    </div>
-
-                    <!-- No hp -->
-                    <div class="mb-3">
-                        <label for="phone" class="form-label">Phone number</label>
-                        <input type="text" class="form-control" id="phone" name="phone">
-                    </div>
-
-                    <!-- Service -->
-                    <p>Add service(s)</p>
-
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="Health protocol" id="healthprot" name="service[]">
-                        <label class="form-check-label" for="healthprot">
-                            Health protocol / Rp25.000
-                        </label>
-                    </div>
-
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="Driver" id="driver" name="service[]">
-                        <label class="form-check-label" for="driver">
-                            Driver / Rp100.000
-                        </label>
-                    </div>
-
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="Fuel filled" id="fuel" name="service[]">
-                        <label class="form-check-label" for="fuel">
-                            Fuel filled / Rp250.000
-                        </label>
-                    </div>
-                    <!-- tombol -->
-                    <br/>
-                    <div class="d-grid gap-2">
-                        <button class="btn btn-primary" type="submit">Book</button>
-                    </div>
-                </form>
+        <!-- Judul -->
+        <section>
+            <div class="container" style = "text-align : center;">
+            <br/>
+                <h4>Rent your car now!</h4>
             </div>
-        </div>
-    </div>
-</section>
+            <br/>
+            <br/>
+        </section>
 
-<!-- Footer -->
-<section>
-    <br/><br/><br/>
-    <div class="bg-light text-center">
-        <p>Created by DANI_1202202071</p>
-    </div>
-</section>
+        <!-- Form dan Gambar -->
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="plisKeTengah">
+                            <img src="https://daihatsu-cilegon.com/wp-content/uploads/2019/03/Daihatsu-allnewterios.png" class="img" alt="Terios">
+                        </div>
+                    </div>
+                    
+                    <div class="col">
+                        <form action="Dani_MyBooking.php" method="POST">
+                            <!-- Nama -->
+                            <div class="mb-3">
+                                <label for="nama" class="form-label">Name</label>
+                                <input type="text" class="form-control" id="nama" name="nama" value="DANI_1202202071" readonly enabled>
+                            </div>
 
-</body>
-</html>
+                            <!-- Tanggal -->
+                            <div class="mb-3">
+                                <label for="bookDate" class="form-label">Book Date</label>
+                                <input type="date" class="form-control" id="bookDate" name="bookDate">
+                            </div>
+
+                            <!-- Waktu -->
+                            <div class="mb-3">
+                                <label for="startTime" class="form-label">Start Time</label>
+                                <input type="time" class="form-control" id="startTime" name="startTime">
+                            </div>
+
+                            <!-- Durasi -->
+                            <div class="mb-3">
+                                <label for="dur" class="form-label">Duration (Days)</label>
+                                <input type="number" class="form-control" id="dur" name="dur">
+                            </div>
+
+                            <!-- Pilih mobil -->
+                            <div class="mb-3">
+                                <label for="car" class="form-label">Car Type</label>
+                                <select class="form-select" aria-label="Default select example" name="car">
+                                    <option selected>Toyota Avanza</option>
+                                    <option value="Daihatsu Terios">Daihatsu Terios</option>
+                                    <option value="Honda Brio">Honda Brio</option>
+                                </select>
+                            </div>
+
+                            <!-- No hp -->
+                            <div class="mb-3">
+                                <label for="phone" class="form-label">Phone number</label>
+                                <input type="text" class="form-control" id="phone" name="phone">
+                            </div>
+
+                            <!-- Service -->
+                            <p>Add service(s)</p>
+
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="Health protocol" id="healthprot" name="service[]">
+                                <label class="form-check-label" for="healthprot">
+                                    Health protocol / Rp25.000
+                                </label>
+                            </div>
+
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="Driver" id="driver" name="service[]">
+                                <label class="form-check-label" for="driver">
+                                    Driver / Rp100.000
+                                </label>
+                            </div>
+
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="Fuel filled" id="fuel" name="service[]">
+                                <label class="form-check-label" for="fuel">
+                                    Fuel filled / Rp250.000
+                                </label>
+                            </div>
+                            <!-- tombol -->
+                            <br/>
+                            <div class="d-grid gap-2">
+                                <button class="btn btn-primary" type="submit">Book</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Footer -->
+        <section>
+            <br/><br/><br/>
+            <div class="bg-light text-center">
+                <p>Created by DANI_1202202071</p>
+            </div>
+        </section>
+
+        </body>
+        </html>
--- Modul 2/Dani_Home.php
+++ Modul 2/Dani_Home.php
@@ -1,120 +1,120 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-        <title>Dani Modul 2</title>
-    </head>
-    <style>
-        .nav-link {
-            color: white;
-        }
-        .nav-link:hover{
-            color: cyan;
-        }
-    </style>
-<body>
-<!-- Navbar -->
-<nav class="navbar navbar-expand-lg bg-dark" >
-    <div class="container-fluid">
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="nav" style="margin-left: auto; margin-right: auto;">
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="Dani_Home.php">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="Dani_Booking.php">Booking</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
-
-<!-- Judul -->
-<section>
-    <div class="container text-center">
-    <br/>
-        <h1>WELCOME TO EAD RENT</h1>
-        <h5>Find your best deal here! <br/><br/></h5>
-    </div>
-</section>
-
-<!-- Card Mobil -->
-<section>
-    <div class="container">
-        <div class="row row-cols-1 row-cols-md-3 g-3">
-            <div class="col">
-                <!-- Avanza -->
-                <div class="card h-100">
-                    <img src="https://static.wixstatic.com/media/bce131_88ac39f3de1b433ca66ccb462f28e6d4~mv2.png/v1/fill/w_688,h_408,al_c/bce131_88ac39f3de1b433ca66ccb462f28e6d4~mv2.png" class="card-img-top h-100" alt="Avanza">
-                    <div class="card-body">
-                        <h5 class="card-title">Toyota Avanza</h5>
-                        <p class="card-text">Rp150.000/Day</p>
-                    </div>
-                    <ul class="list-group list-group-flush text-center">
-                        <li class="list-group-item">8 Kursi</li>
-                        <li class="list-group-item">1500 cc</li>
-                        <li class="list-group-item">Manual</li>
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+                <title>Dani Modul 2</title>
+            </head>
+            <style>
+                .nav-link {
+                    color: white;
+                }
+                .nav-link:hover{
+                    color: cyan;
+                }
+            </style>
+        <body>
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg bg-dark" >
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="nav" style="margin-left: auto; margin-right: auto;">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="Dani_Home.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="Dani_Booking.php">Booking</a>
+                        </li>
                     </ul>
-                    <div class="card-footer text-center">
-                        <div class="d-grid gap-2 d-md-block">
-                            <button class="btn btn-primary" type="button">Book Now</button>
-                        </div>
-                    </div>
                 </div>
             </div>
-            <div class="col">
-                <!-- Terios -->
-                <div class="card h-100">
-                <img src="https://daihatsu-cilegon.com/wp-content/uploads/2019/03/Daihatsu-allnewterios.png" class="card-img-top h-100" alt="Terios">
-                    <div class="card-body">
-                        <h5 class="card-title">Daihatsu Terios</h5>
-                        <p class="card-text">Rp200.000/Day</p>
-                    </div>
-                    <ul class="list-group list-group-flush text-center">
-                        <li class="list-group-item">7 Kursi</li>
-                        <li class="list-group-item">1500 cc</li>
-                        <li class="list-group-item">Manual</li>
-                    </ul>
-                    <div class="card-footer text-center">
-                        <div class="d-grid gap-2 d-md-block">
-                            <a href="Dani_Booking.php" class="btn btn-primary" type="button">Book Now</a>
+        </nav>
+
+        <!-- Judul -->
+        <section>
+            <div class="container text-center">
+            <br/>
+                <h1>WELCOME TO EAD RENT</h1>
+                <h5>Find your best deal here! <br/><br/></h5>
+            </div>
+        </section>
+
+        <!-- Card Mobil -->
+        <section>
+            <div class="container">
+                <div class="row row-cols-1 row-cols-md-3 g-3">
+                    <div class="col">
+                        <!-- Avanza -->
+                        <div class="card h-100">
+                            <img src="https://static.wixstatic.com/media/bce131_88ac39f3de1b433ca66ccb462f28e6d4~mv2.png/v1/fill/w_688,h_408,al_c/bce131_88ac39f3de1b433ca66ccb462f28e6d4~mv2.png" class="card-img-top h-100" alt="Avanza">
+                            <div class="card-body">
+                                <h5 class="card-title">Toyota Avanza</h5>
+                                <p class="card-text">Rp150.000/Day</p>
+                            </div>
+                            <ul class="list-group list-group-flush text-center">
+                                <li class="list-group-item">8 Kursi</li>
+                                <li class="list-group-item">1500 cc</li>
+                                <li class="list-group-item">Manual</li>
+                            </ul>
+                            <div class="card-footer text-center">
+                                <div class="d-grid gap-2 d-md-block">
+                                    <button class="btn btn-primary" type="button">Book Now</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div class="col">
-                <!-- Brio -->
-                <div class="card h-100">
-                <img src="https://asset.honda-indonesia.com/variants/images/Vos4HUL6XFLHnM2dhTzGbzdNHpvNTnBNBKhZwxc0.png" class="card-img-top h-100" alt="Brio">
-                    <div class="card-body">
-                        <h5 class="card-title">Honda Brio</h5>
-                        <p class="card-text">Rp100.000/Day</p>
-                    </div>
-                    <ul class="list-group list-group-flush text-center">
-                        <li class="list-group-item">5 Kursi</li>
-                        <li class="list-group-item">1200 cc</li>
-                        <li class="list-group-item">Manual</li>
-                    </ul>
-                    <div class="card-footer text-center">
-                        <div class="d-grid gap-2 d-md-block">
-                            <button class="btn btn-primary" type="button">Book Now</button>
+                    <div class="col">
+                        <!-- Terios -->
+                        <div class="card h-100">
+                        <img src="https://daihatsu-cilegon.com/wp-content/uploads/2019/03/Daihatsu-allnewterios.png" class="card-img-top h-100" alt="Terios">
+                            <div class="card-body">
+                                <h5 class="card-title">Daihatsu Terios</h5>
+                                <p class="card-text">Rp200.000/Day</p>
+                            </div>
+                            <ul class="list-group list-group-flush text-center">
+                                <li class="list-group-item">7 Kursi</li>
+                                <li class="list-group-item">1500 cc</li>
+                                <li class="list-group-item">Manual</li>
+                            </ul>
+                            <div class="card-footer text-center">
+                                <div class="d-grid gap-2 d-md-block">
+                                    <a href="Dani_Booking.php" class="btn btn-primary" type="button">Book Now</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
+                    <div class="col">
+                        <!-- Brio -->
+                        <div class="card h-100">
+                        <img src="https://asset.honda-indonesia.com/variants/images/Vos4HUL6XFLHnM2dhTzGbzdNHpvNTnBNBKhZwxc0.png" class="card-img-top h-100" alt="Brio">
+                            <div class="card-body">
+                                <h5 class="card-title">Honda Brio</h5>
+                                <p class="card-text">Rp100.000/Day</p>
+                            </div>
+                            <ul class="list-group list-group-flush text-center">
+                                <li class="list-group-item">5 Kursi</li>
+                                <li class="list-group-item">1200 cc</li>
+                                <li class="list-group-item">Manual</li>
+                            </ul>
+                            <div class="card-footer text-center">
+                                <div class="d-grid gap-2 d-md-block">
+                                    <button class="btn btn-primary" type="button">Book Now</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
             </div>
-    </div>
-</section>
+        </section>
 
-<br/>
-<br/>
+        <br/>
+        <br/>
 
-<!-- Footer -->
-<section>
-    <div class="bg-light text-center">
-        <p>Created by DANI_1202202071</p>
-    </div>
-</section>
+        <!-- Footer -->
+        <section>
+            <div class="bg-light text-center">
+                <p>Created by DANI_1202202071</p>
+            </div>
+        </section>
 
-</body>
-</html>
+        </body>
+        </html>
